84baf75125eab3c7c6973eacd3f77306236d2392
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-07-12 16:05:28 +0530

    modified:   src/components/Hist.js 	modified:   src/components/ImageUpload.js

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -66,7 +66,7 @@ export default function ImageUpload() {
 
       try {
         const { data } = await axios.post(
-          'http://localhost:5001/api/yolov5',
+          'https://69a0-2409-408c-1414-77ae-349f-579b-7ee0-17c8.ngrok.io/api/yolov5',
           formData,
           config
         );
@@ -101,7 +101,7 @@ export default function ImageUpload() {
 
       try {
         const { data } = await axios.post(
-          'http://localhost:5001/api/check-image',
+          'https://69a0-2409-408c-1414-77ae-349f-579b-7ee0-17c8.ngrok.io/api/check-image',
           formData,
           config
         );
