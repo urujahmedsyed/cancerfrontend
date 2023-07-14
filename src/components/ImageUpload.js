7da49075b0b28135ca44258bf3b99e9077565c75
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-07-14 16:28:14 +0530

    modified:   src/components/ImageUpload.js

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -120,51 +120,21 @@ export default function ImageUpload() {
     }
   };
 
+  const calculateAllredScore = (counts) => {
+    const prop = (counts[1] + counts[2] + counts[3]) / (counts[0] + counts[1] + counts[2] + counts[3]);
+    const inten = Math.floor((counts[1] + counts[2] * 2 + counts[3] * 3) / (counts[1] + counts[2] + counts[3]));
+    const proportion = prop === 0 ? 0 : prop <= 0.01 ? 1 : prop <= 0.1 ? 2 : prop <= 0.33 ? 3 : prop <= 0.66 ? 4 : 5;
+    const intensity = counts[1] + counts[2] + counts[3] === 0 ? 0 : inten;
+    const allred = proportion + intensity;
+    setAllredScore(allred);
+  };
+
   useEffect(() => {
-    const calculateAllredScore = (counts) => {
-      const prop = (counts[1] + counts[2] + counts[3]) / (counts[0] + counts[1] + counts[2] + counts[3]);
-      const proportion = Math.round(prop * 100);
-  
-      let proportionScore;
-      if (proportion === 0) {
-        proportionScore = 0;
-      } else if (proportion < 1) {
-        proportionScore = 1;
-      } else if (proportion >= 1 && proportion <= 10) {
-        proportionScore = 2;
-      } else if (proportion > 10 && proportion <= 33) {
-        proportionScore = 3;
-      } else if (proportion > 33 && proportion <= 66) {
-        proportionScore = 4;
-      } else {
-        proportionScore = 5;
-      }
-  
-      let intensity;
-      if (counts[0] >= counts[1] && counts[0] >= counts[2] && counts[0] >= counts[3]) {
-        intensity = 0;
-      } else if (counts[1] >= counts[0] && counts[1] >= counts[2] && counts[1] >= counts[3]) {
-        intensity = 1;
-      } else if (counts[2] >= counts[0] && counts[2] >= counts[1] && counts[2] >= counts[3]) {
-        intensity = 2;
-      } else {
-        intensity = 3;
-      }
-  
-      const allred = proportionScore + intensity;
-      setAllredScore(allred);
-      console.log(proportionScore);
-      console.log(intensity);
-      console.log(allred);
-    };
-  
-    if (response && response.n && response.w && response.m && response.s && allredScore === null) {
+    if (response && response.n && response.w && response.m && response.s) {
       const counts = [response.n, response.w, response.m, response.s];
       calculateAllredScore(counts);
     }
-  }, [response, allredScore]);
-  
-  
+  }, [response]);
 
   return (
     <>
