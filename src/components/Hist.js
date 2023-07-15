1524d859c624d54923d6e732281a89d513c857a5
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-07-15 08:34:56 +0530

    modified:   src/components/Hist.js

--- src/components/Hist.js
+++ src/components/Hist.js
@@ -35,7 +35,7 @@ const Hist = () => {
     formData.append('input_image', selectedFile);
 
     axios
-      .post('https://b244-2409-408c-1405-265-4f3-e610-a8ec-3c32.ngrok-free.app /api/execute-notebook', formData)
+      .post('https://b244-2409-408c-1405-265-4f3-e610-a8ec-3c32.ngrok-free.app/api/execute-notebook', formData)
       .then((response) => {
         setResult(response.data);
         setMaskOverlayImage(`data:image/png;base64,${response.data.mask_overlay}`);
