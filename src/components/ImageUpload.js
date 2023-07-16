cb6ecd76a8691a42ba9187c9fe5681598efc8d85
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-07-16 14:41:55 +0530

    modified:   src/components/ImageUpload.js

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -241,7 +241,7 @@ export default function ImageUpload() {
                   </h3>
                   <br></br>
                   <h3>
-                    AllRed Score: <span>{allredScore}</span>
+                    AllRed Score: <span>{calculateAllredScore(response)}</span>
                   </h3>
                 </div>
                 <br></br>
