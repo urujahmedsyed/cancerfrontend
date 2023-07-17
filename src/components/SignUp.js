dcc27ae646c72fbf33fec85338994d6eb564f5ef
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-07-17 18:39:23 +0530

    modified:   src/components/SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,6 +12,28 @@ function SignUp() {
     const [mobile, setMobile] = useState('');
     const [password, setPassword] = useState('');
     const [hospital, setHospital] = useState('');
+    const [otp, setOtp] = useState('');
+    const [isOtpSent, setIsOtpSent] = useState(false);
+
+    async function sendOtp() {
+        const response = await fetch('https://cancerserver.onrender.com/api/send-otp', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+            }),
+        });
+
+        const data = await response.json();
+        if (data.status === 'ok') {
+            setIsOtpSent(true);
+            window.alert('OTP sent successfully!');
+        } else {
+            window.alert('Failed to send OTP. Please try again!');
+        }
+    }
 
     async function registerUser(event) {
         event.preventDefault();
@@ -27,6 +49,7 @@ function SignUp() {
                 mobile,
                 password,
                 hospital,
+                otp,
             }),
         });
 
@@ -49,71 +72,87 @@ function SignUp() {
                     <h2><br/>User Signup</h2>
                 </div>
                 <div id="loginfrms21">
-                    <form onSubmit={registerUser}>
-                        <div class="mb-3">
-                            <label for="exampleInputName1" class="form-label">Name</label>
-                            <input 
-                                value={name}
-                                onChange={(e)=>setName(e.target.value)}
-                                type="text"
-                                class="form-control"
-                                id="exampleInputName1"
-                                placeholder="Name"/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email</label>
-                            <input
-                                value={email}
-                                onChange={(e)=>setEmail(e.target.value)}
-                                type="email"
-                                class="form-control"
-                                id="exampleInputEmail1"
-                                placeholder="Email"/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputUsername1"class="form-label">Username</label>
-                            <input
-                                value={uname}
-                                onChange={(e)=>setUname(e.target.value)}
-                                type="text"
-                                class="form-control"
-                                id="exampleInputUsername1"
-                                placeholder="Username"/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputHospital1"class="form-label">Hospital</label>
-                            <input
-                                value={hospital}
-                                onChange={(e)=>setHospital(e.target.value)}
-                                type="text"
-                                class="form-control"
-                                id="exampleInputHospital1"
-                                placeholder="Hospital"/>
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input
-                                value={password}
-                                onChange={(e)=>setPassword(e.target.value)}
-                                type="password"
-                                class="form-control"
-                                id="exampleInputPassword1"
-                                placeholder="Password"/>
-                        </div>
-                        
-                        <div class="mb-3">
-                            <label for="exampleInputMobileNo1" class="form-label">Mobile No.</label>
-                            <input
-                                value={mobile}
-                                onChange={(e)=>setMobile(e.target.value)}
-                                type="number"
-                                class="form-control"
-                                id="exampleInputMobileNo1"
-                                placeholder="Mobile Number"/>
-                        </div>
-                        <br/>
-                        <button type="submit" class="btn btn-primary">Sign Up</button>
-                    </form>
+                    {!isOtpSent ? (
+                        <form onSubmit={sendOtp}>
+                            <div class="mb-3">
+                                <label for="exampleInputName1" class="form-label">Name</label>
+                                <input 
+                                    value={name}
+                                    onChange={(e)=>setName(e.target.value)}
+                                    type="text"
+                                    class="form-control"
+                                    id="exampleInputName1"
+                                    placeholder="Name"/>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email</label>
+                                <input
+                                    value={email}
+                                    onChange={(e)=>setEmail(e.target.value)}
+                                    type="email"
+                                    class="form-control"
+                                    id="exampleInputEmail1"
+                                    placeholder="Email"/>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputUsername1"class="form-label">Username</label>
+                                <input
+                                    value={uname}
+                                    onChange={(e)=>setUname(e.target.value)}
+                                    type="text"
+                                    class="form-control"
+                                    id="exampleInputUsername1"
+                                    placeholder="Username"/>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputHospital1"class="form-label">Hospital</label>
+                                <input
+                                    value={hospital}
+                                    onChange={(e)=>setHospital(e.target.value)}
+                                    type="text"
+                                    class="form-control"
+                                    id="exampleInputHospital1"
+                                    placeholder="Hospital"/>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input
+                                    value={password}
+                                    onChange={(e)=>setPassword(e.target.value)}
+                                    type="password"
+                                    class="form-control"
+                                    id="exampleInputPassword1"
+                                    placeholder="Password"/>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputMobileNo1" class="form-label">Mobile No.</label>
+                                <input
+                                    value={mobile}
+                                    onChange={(e)=>setMobile(e.target.value)}
+                                    type="number"
+                                    class="form-control"
+                                    id="exampleInputMobileNo1"
+                                    placeholder="Mobile Number"/>
+                            </div>
+                            <br/>
+                            <button type="submit" class="btn btn-primary">Send OTP</button>
+                        </form>
+                    ) : (
+                        <form onSubmit={registerUser}>
+                            <div class="mb-3">
+                                <label for="exampleInputOTP" class="form-label">Enter OTP</label>
+                                <input
+                                    value={otp}
+                                    onChange={(e)=>setOtp(e.target.value)}
+                                    type="text"
+                                    class="form-control"
+                                    id="exampleInputOTP"
+                                    placeholder="OTP"/>
+                            </div>
+                            <br/>
+                            <button type="submit" class="btn btn-primary">Verify OTP &amp; Sign Up</button>
+                        </form>
+                    )}
                 </div>
             </div>
             <br/>
