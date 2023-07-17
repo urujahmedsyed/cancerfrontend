cb55eb06246b8553e9d09c3dce5bdf01b7ee199a
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-07-17 23:53:49 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,234 +1,261 @@
-import React from "react";
-import "../styles/signup.css";
-import Navu from "./Navu";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase.config";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import OtpInput from "otp-input-react";
+import { CgSpinner } from "react-icons/cg";
+import { BsFillShieldLockFill, BsEnvelopeFill } from "react-icons/bs";
+import { toast, Toaster } from "react-hot-toast";
 
-function SignUp() {
-  const history = useNavigate();
+const SignUp = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [uname, setUname] = useState("");
   const [mobile, setMobile] = useState("");
   const [password, setPassword] = useState("");
   const [hospital, setHospital] = useState("");
+  const [selectedOption, setSelectedOption] = useState(""); // Track selected OTP option
   const [otpSent, setOtpSent] = useState(false); // Track if OTP has been sent
-  const [otp, setOtp] = useState("");
-  const [otpReceived, setOtpReceived] = useState(""); // Store the received OTP
+  const [otp, setOtp] = useState(""); // Store the entered OTP
+  const [loading, setLoading] = useState(false);
 
-  async function sendOtp() {
-    try {
-      const response = await fetch(
-        "https://cancerserver.onrender.com/api/send-otp",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email,
-          }),
-        }
-      );
-
-      const data = await response.json();
-      if (data.status === "ok") {
-        window.alert("OTP sent successfully!");
-        setOtpReceived(data.otp); // Store the received OTP in a separate state variable
-        setOtpSent(true); // Set OTP sent state to true
-      } else {
-        window.alert("Failed to send OTP. Please try again!");
-      }
-    } catch (error) {
-      console.error("Error sending OTP:", error);
-      window.alert("Failed to send OTP. Please try again!");
+  async function sendOTP() {
+    if (selectedOption === "email") {
+      sendEmailOTP();
+    } else if (selectedOption === "phone") {
+      sendPhoneOTP();
     }
   }
 
-  async function verifyOtp(event) {
-    event.preventDefault();
-    if (otp === otpReceived) {
-      // OTP verification successful
-      window.alert("OTP verified!");
-      setOtp(""); // Reset OTP state
-      registerUser();
-    } else {
-      // OTP verification failed
-      window.alert("Invalid OTP. Please try again!");
+  async function sendEmailOTP() {
+    try {
+      const response = await fetch("https://cancerserver.onrender.com/api/send-otp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+      if (data.status === "ok") {
+        toast.success("OTP sent successfully to your email!");
+        setOtpSent(true);
+      } else {
+        toast.error("Failed to send OTP. Please try again!");
+      }
+    } catch (error) {
+      console.error("Error sending OTP:", error);
+      toast.error("Failed to send OTP. Please try again!");
     }
+  }
+
+  async function sendPhoneOTP() {
+    try {
+      setLoading(true);
+      const formatMobile = `+${mobile}`;
+      const appVerifier = window.recaptchaVerifier;
+
+      const confirmationResult = await auth.signInWithPhoneNumber(formatMobile, appVerifier);
+      window.confirmationResult = confirmationResult;
+      setLoading(false);
+      toast.success("OTP sent successfully to your phone number!");
+      setOtpSent(true);
+    } catch (error) {
+      console.error("Error sending OTP:", error);
+      setLoading(false);
+      toast.error("Failed to send OTP. Please try again!");
+    }
+  }
+
+  async function verifyOTP() {
+    setLoading(true);
+    if (selectedOption === "email") {
+      verifyEmailOTP();
+    } else if (selectedOption === "phone") {
+      verifyPhoneOTP();
+    }
+  }
+
+  async function verifyEmailOTP() {
+    try {
+      const response = await fetch("https://cancerserver.onrender.com/api/verify-otp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ otp, email }),
+      });
+
+      const data = await response.json();
+      if (data.status === "verified") {
+        toast.success("OTP verified!");
+        registerUser();
+      } else {
+        toast.error("Invalid OTP. Please try again!");
+      }
+    } catch (error) {
+      console.error("Error verifying OTP:", error);
+      toast.error("Failed to verify OTP. Please try again!");
+    }
+    setLoading(false);
+  }
+
+  async function verifyPhoneOTP() {
+    try {
+      const result = await window.confirmationResult.confirm(otp);
+      if (result.user) {
+        toast.success("OTP verified!");
+        registerUser();
+      } else {
+        toast.error("Invalid OTP. Please try again!");
+      }
+    } catch (error) {
+      console.error("Error verifying OTP:", error);
+      toast.error("Failed to verify OTP. Please try again!");
+    }
+    setLoading(false);
   }
 
   async function registerUser() {
     try {
-      const response = await fetch(
-        "https://cancerserver.onrender.com/api/signup",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name,
-            email,
-            uname,
-            mobile,
-            password,
-            hospital,
-            otp: otpReceived, // Use the received OTP for registration
-          }),
-        }
-      );
+      const response = await fetch("https://cancerserver.onrender.com/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          uname,
+          mobile,
+          password,
+          hospital,
+          otp,
+        }),
+      });
 
       const data = await response.json();
       if (data.status === "yaya") {
-        // Display success alert
-        window.alert("Signup successful!");
-        history.push("/login");
+        toast.success("Signup successful!");
+        navigate("/login");
       } else {
-        // Display invalid signup alert
-        window.alert("Invalid signup!");
+        toast.error("Invalid signup!");
       }
     } catch (error) {
       console.error("Error registering user:", error);
-      window.alert("Failed to register user. Please try again!");
+      toast.error("Failed to register user. Please try again!");
     }
   }
 
   return (
-    <>
-      <div id="nav1">
-        <Navu />
-      </div>
-      <div class="container" id="dome21">
-        <div class="container" id="resdesc21">
-          <h2>
-            <br />
-            User Signup
-          </h2>
-        </div>
-        <div id="loginfrms21">
-          <form>
-            <div class="mb-3">
-              <label for="exampleInputName1" class="form-label">
-                Name
-              </label>
-              <input
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                type="text"
-                class="form-control"
-                id="exampleInputName1"
-                placeholder="Name"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
+    <section className="bg-emerald-500 flex items-center justify-center h-screen">
+      <div>
+        <Toaster toastOptions={{ duration: 4000 }} />
+
+        <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
+          <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
+            BREAST CANCER DETECTION PORTAL SIGNUP
+          </h1>
+
+          <div className="flex items-center justify-center">
+            <button
+              className={`btn-option ${
+                selectedOption === "email" ? "btn-option-active" : ""
+              }`}
+              onClick={() => setSelectedOption("email")}
+            >
+              <BsEnvelopeFill size={20} />
+              <span>Email</span>
+            </button>
+
+            <button
+              className={`btn-option ${
+                selectedOption === "phone" ? "btn-option-active" : ""
+              }`}
+              onClick={() => setSelectedOption("phone")}
+            >
+              <BsFillShieldLockFill size={20} />
+              <span>Phone</span>
+            </button>
+          </div>
+
+          {selectedOption === "email" && (
+            <div>
+              <label htmlFor="inputEmail" className="form-label">
                 Email
               </label>
               <input
+                type="email"
+                className="form-control"
+                id="inputEmail"
+                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                placeholder="Email"
               />
             </div>
-            <div class="mb-3">
-              <label for="exampleInputUsername1" class="form-label">
-                Username
+          )}
+
+          {selectedOption === "phone" && (
+            <div>
+              <label htmlFor="inputPhone" className="form-label">
+                Phone Number
               </label>
-              <input
-                value={uname}
-                onChange={(e) => setUname(e.target.value)}
-                type="text"
-                class="form-control"
-                id="exampleInputUsername1"
-                placeholder="Username"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputHospital1" class="form-label">
-                Hospital
-              </label>
-              <input
-                value={hospital}
-                onChange={(e) => setHospital(e.target.value)}
-                type="text"
-                class="form-control"
-                id="exampleInputHospital1"
-                placeholder="Hospital"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
-                Password
-              </label>
-              <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                class="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputMobileNo1" class="form-label">
-                Mobile No.
-              </label>
-              <input
+              <PhoneInput
+                country={"in"}
                 value={mobile}
-                onChange={(e) => setMobile(e.target.value)}
-                type="number"
-                class="form-control"
-                id="exampleInputMobileNo1"
-                placeholder="Mobile Number"
+                onChange={(phone) => setMobile(phone)}
+                inputProps={{
+                  id: "inputPhone",
+                  required: true,
+                }}
               />
             </div>
-            <br />
-            {!otpSent && ( // Render the Send OTP button only if OTP has not been sent
-              <button
-                type="button"
-                class="btn btn-primary"
-                onClick={sendOtp}
-              >
-                Send OTP
-              </button>
-            )}
-            {otpSent && ( // Render the Verify OTP and Sign Up button if OTP has been sent
-              <>
-                <br />
-                <br />
-                <div class="mb-3">
-                  <label for="exampleInputOTP" class="form-label">
-                    Enter OTP
-                  </label>
-                  <input
-                    value={otp}
-                    onChange={(e) => setOtp(e.target.value)}
-                    type="text"
-                    class="form-control"
-                    id="exampleInputOTP"
-                    placeholder="OTP"
-                  />
-                </div>
-                <button
-                  type="button"
-                  class="btn btn-primary"
-                  onClick={verifyOtp}
-                >
-                  Verify OTP & Sign Up
-                </button>
-              </>
-            )}
-          </form>
+          )}
+
+          {otpSent && (
+            <div className="mb-3">
+              <label htmlFor="inputOTP" className="form-label">
+                Enter OTP
+              </label>
+              <OtpInput
+                value={otp}
+                onChange={setOtp}
+                numInputs={6}
+                separator={<span>-</span>}
+                isInputNum
+                shouldAutoFocus
+              />
+            </div>
+          )}
+
+          {!otpSent && (
+            <button
+              type="button"
+              className="btn btn-primary w-full"
+              onClick={sendOTP}
+            >
+              Send OTP
+            </button>
+          )}
+
+          {otpSent && (
+            <button
+              type="button"
+              className="btn btn-primary w-full"
+              onClick={verifyOTP}
+              disabled={loading}
+            >
+              {loading ? <CgSpinner className="animate-spin" size={20} /> : null}
+              <span>Verify OTP & Sign Up</span>
+            </button>
+          )}
         </div>
       </div>
-      <br />
-      <br />
-    </>
+    </section>
   );
-}
+};
 
 export default SignUp;
